10bf39e1198f8716bb1ce5c715d579eddc80e667
Fr3myXS <antonovch13@mail.ru>
2020-11-19 17:02:28 +0500

    ReadListWords

---
+++ ReadListWords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReadFile
+{
+    public class ReadListWords
+    {
+        private string text = File.ReadAllText(@"\..\word_rus.txt");
+        public List<string> Words = new List<string>();
+
+        public object Dictionary()
+        {
+            Words.AddRange(text.Split('\n'));
+
+            return Words;
+        }
+    }
+}
